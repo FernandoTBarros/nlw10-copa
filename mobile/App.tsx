29de23f6a9e963184f035984c3644dcd4cbf2577
Fernando T. Barros <fernando@deltasi.com.br>
2022-11-07 01:32:30 -0300

    Finalizando aula 3, 4 e metade da 5

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,15 @@
-import { NativeBaseProvider, StatusBar } from "native-base";
 import {
-  useFonts,
   Roboto_400Regular,
   Roboto_500Medium,
   Roboto_700Bold,
+  useFonts,
 } from "@expo-google-fonts/roboto";
+import { NativeBaseProvider, StatusBar } from "native-base";
 
-import { THEME } from "./src/styles/theme";
 import { Loading } from "./src/components/Loading";
-import { SignIn } from "./src/screens/SignIn";
+import { AuthContextProvider } from "./src/context/AuthContext";
+import { Routes } from "./src/routes";
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,12 +20,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
